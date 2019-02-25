fa820ca0f137c069c4395fe6477a147378dbd366
Benjamin Hall <BHall2@myjmc.edu.au>
2019-02-26 09:14:56 +1100

    Updated scene

---
+++ RapidPrototyping/Assets/Scripts/TimeCycle.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TimeCycle : MonoBehaviour
+{
+
+    public enum TimeOfDay {MorningTime, DayTime, AfternoonTime, NightTime }
+
+    public bool ChangeTimeRandom;
+    bool stopped;
+    public TimeOfDay timeOfDay;
+    Light lightSource;
+    float transitionTime = 2f;
+
+    [Header("Sky Colours")]
+    public Color morningColour;
+    public Color dayTimeColour;
+    public Color afternoonColour;
+    public Color nightColour;
+
+    [Header("Timers")]
+    public int currentTime;
+    public float timeModifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeOfDay = TimeOfDay.MorningTime;
+        lightSource = GetComponent<Light>();
+        Debug.Log(Utilities.EnumLength(timeOfDay));
+        currentTime = 0;
+        StartCoroutine(TickTock());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            ChangeTime();
+        if (Input.GetKeyDown(KeyCode.R))
+            ChangeTime();
+    }
+
+    void ChangeTime()
+    {
+        if(ChangeTimeRandom)
+        {
+            timeOfDay = Utilities.RandomEnumValue<TimeOfDay>();
+        }
+
+        if ((int)timeOfDay == Utilities.EnumLength(timeOfDay))
+            timeOfDay = 0;
+        else
+        timeOfDay++;
+
+        Debug.Log(Utilities.EnumToString(timeOfDay));
+        
+        switch (timeOfDay)
+        {
+            case TimeOfDay.MorningTime:
+                lightSource.DOColor(morningColour, transitionTime);
+                break;
+            case TimeOfDay.DayTime:
+                lightSource.DOColor(dayTimeColour, transitionTime);
+                break;
+            case TimeOfDay.AfternoonTime:
+                lightSource.DOColor(afternoonColour, transitionTime);
+                break;
+            case TimeOfDay.NightTime:
+                lightSource.DOColor(nightColour, transitionTime);
+                break;
+        }
+    }
+
+    IEnumerator TickTock()
+    {
+        while (!stopped)
+        {
+            yield return new WaitForSeconds(timeModifier);
+            if (currentTime == 23)
+                currentTime = 0;
+            else
+                currentTime++;
+        }
+    }
+
+    
+}
---
+++ RapidPrototyping/Assets/Scripts/Utilities.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System;
+
+public static class Utilities 
+{
+    /// <summary>
+    /// returns the name of a given enum.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+
+    public static int EnumLength(this Enum value)
+    {
+        Type t = value.GetType();
+        return Enum.GetValues(t).Length - 1;
+    }
+
+    public static string EnumToString(this Enum value)
+    {
+        var stringVal = value.ToString();
+        var bld = new StringBuilder();
+
+        for (var i = 0; i < stringVal.Length; i++)
+        {
+            if(char.IsUpper(stringVal[i]))
+            {
+                bld.Append(" ");
+            }
+            bld.Append(stringVal[i]);
+        }
+
+        bld.Remove(0, 1);
+
+        return bld.ToString();
+        
+    }
+    
+    public static T RandomEnumValue<T>()
+    {
+        var values = Enum.GetValues(typeof(T));
+        int random = UnityEngine.Random.Range(0, values.Length);
+        return (T)values.GetValue(random);
+    }
+}
